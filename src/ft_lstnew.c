ff3f06920d7ceb82b7fe7f20233c67a7a819a809
Amine <aazri@student.42.fr>
2017-09-17 13:28:14 +0200

    Fix malloc, i didn't have space for a NULL termating character wich caused an overflow with my ft_memcpy.

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -17,11 +17,11 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*new;
 
-	if ((new = malloc(sizeof(*new))) == NULL)
+	if ((new = ft_memalloc(sizeof(*new))) == NULL)
 		return (NULL);
 	if (content)
 	{
-		if ((new->content = malloc(content_size)) == NULL)
+		if ((new->content = ft_memalloc(content_size + 1)) == NULL)
 		{
 			free(new);
 			return (NULL);
